4b7090d03a7f0cd2229b6bcb54bd0cf938b902ba
chanveda <51695027+chanveda@users.noreply.github.com>
2019-11-29 18:23:02 +0530

    Add files via upload

---
+++ ConstructionLine.CodingChallenge.Tests/SearchEnginePerformanceTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ConstructionLine.CodingChallenge.Tests.SampleData;
+using NUnit.Framework;
+
+namespace ConstructionLine.CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchEnginePerformanceTests : SearchEngineTestsBase
+    {
+        private List<Shirt> _shirts;
+        private SearchEngine _searchEngine;
+
+        [SetUp]
+        public void Setup()
+        {
+            var dataBuilder = new SampleDataBuilder(50000);
+
+            _shirts = dataBuilder.CreateShirts();
+
+            _searchEngine = new SearchEngine(_shirts);
+        }
+
+
+        [Test]
+        public void PerformanceTest()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var options = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Red }
+            };
+
+            var results = _searchEngine.Search(options);
+            // var results = _searchEngine.LookupSearch(options);
+
+
+            sw.Stop();
+            Console.WriteLine($"Test fixture finished in {sw.ElapsedMilliseconds} milliseconds");
+            Console.WriteLine($"Lookup intialization finished in {results.Milliseconds} milliseconds");
+            Console.WriteLine($"Search finished in {sw.ElapsedMilliseconds - results.Milliseconds} milliseconds");
+
+
+
+            AssertResults(results.Shirts, options);
+            AssertSizeCounts(_shirts, options, results.SizeCounts);
+            AssertColorCounts(_shirts, options, results.ColorCounts);
+        }
+    }
+}
